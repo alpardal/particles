8fb5a8bc4c8a564ec4b5d39b202f77c48068d39f
André Luiz Pardal <alpardal@gmail.com>
2013-08-24 14:28:26 -0300

    change particle color according to velocity

--- particle_renderer.js
+++ particle_renderer.js
@@ -1,14 +1,24 @@
 ParticleRenderer = function(particles) {
     var particleSize = 1;
+    var lower = 3.8, upper = 8;
 
-    var drawParticleAt = function(position, ctx) {
+    var getColor = function(velocity) {
+        var mag = velocity.getMagnitude();
+        var delta = mag <= lower ? 0 : (mag >= upper ? 1 : mag - lower);
+        var c = Math.floor(delta * 255);
+        var color =  'rgb(' + c + ', ' + c + ', 255)';
+        return color;
+    };
+
+    var drawParticleAt = function(particle, ctx) {
+        var position = particle.position;
+        ctx.fillStyle = getColor(particle.velocity);
         ctx.fillRect(position.x, position.y, particleSize, particleSize);
     };
 
     this.render = function(ctx) {
-        ctx.fillStyle = 'rgb(0, 0, 255)';
         for (var i = 0; i < particles.length; i++) {
-            drawParticleAt(particles[i].position, ctx);
+            drawParticleAt(particles[i], ctx);
         }
     }
 }
