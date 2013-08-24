36e6d212d090d4c498a7fb977f4ca2b2b17432c5
André Luiz Pardal <alpardal@gmail.com>
2013-08-24 13:34:26 -0300

    extract speed variable

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 (function() {
     var canvas = new ParticleCanvas('#particles-canvas');
     var world = new World(canvas.bounds);
+    var speed = 4;
 
     world.addEmitter(new Emitter(new Vector(canvas.width/8,
                                             canvas.height*7/8),
-                                 Vector.fromAngle(-Math.PI/8, 4)));
+                                 Vector.fromAngle(-Math.PI/8, speed)));
     world.addEmitter(new Emitter(new Vector(canvas.width/8,
                                             canvas.height/8),
-                                 Vector.fromAngle(Math.PI/8, 4)));
+                                 Vector.fromAngle(Math.PI/8, speed)));
     world.addEmitter(new Emitter(new Vector(canvas.width-50, canvas.height-50),
-                                 Vector.fromAngle(Math.PI*1.25, 4)));
+                                 Vector.fromAngle(Math.PI*1.25, speed)));
 
      var randomField = function() {
          var pos = new Vector(canvas.width * Math.random(),
